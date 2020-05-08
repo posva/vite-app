52a69bf692fbe1187b3443f8bb7df9fb5857050c
Andrew <andrew.seepy@gmail.com>
2020-05-08 10:51:43 +0800

    .

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Hello Vite + Vue 3!</h1>
+  <p>Edit ./App.vue to test hot module replacement (HMR).</p>
+  <p>
+    <span>Count is: {{ count }}</span>
+    <button @click="count++">increment</button>
+  </p>
+  <div>
+    <hr />
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <hr />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({ count: 0 }),
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #4fc08d;
+}
+
+h1,
+p {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <div> -->
+    <h1>Home</h1>
+    <div>
+      <span>count: {{ count }}</span>
+      <button @click="increment()">increment</button>
+    </div>
+  <!-- </div> -->
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Home",
+
+  setup() {
+    const count = ref(0);
+    function increment() {
+      count.value++;
+    }
+
+    return { count, increment };
+  },
+};
+</script>
